b2aec7d99c158b810b3d4505dbf0cf2ef56f9b87
Simon I. Reilly <sir32181@gmail.com>
2014-09-12 21:12:59 +0100

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,10 +19,39 @@ describe User do
 	it { should respond_to(:admin) }
 	it { should respond_to(:microposts) }
 	it { should respond_to(:feed) }
-
+	it { should respond_to(:relationships) }
+	it { should respond_to(:followed_users) }
+	it { should respond_to(:reverse_relationships) }
+	it { should respond_to(:followers) }
+	it { should respond_to(:following?) }
+	it { should respond_to(:follow!) }
+	it { should respond_to(:unfollow!) }
 
 	it {  should be_valid }
-	 it { should_not be_admin }
+	it { should_not be_admin }
+
+	describe "following" do
+		let(:other_user) { FactoryGirl.create(:user) }
+		before do
+			@user.save
+			@user.follow!(other_user)
+		end
+	    
+	    it { should be_following(other_user) }
+	    its(:followed_users) { should include(other_user) }
+
+	    describe "followed user" do
+	      subject { other_user }
+	      its(:followers) { should include(@user) }
+	    end
+
+		describe "and unfollowing" do
+	      	before { @user.unfollow!(other_user) }
+
+	      	it { should_not be_following(other_user) }
+	      	its(:followed_users) { should_not include(other_user) }
+	    end
+	end
 
   	describe "with admin attribute set to 'true'" do
 	    before do
@@ -35,10 +64,10 @@ describe User do
 
 	#Password
 	describe "When password is not present" do
-		  before do
+		before do
     		@user = User.new(name: "Example User", email: "user@example.com",
                      password: " ", password_confirmation: " ")
-  			end
+  		end
   		it { should_not be_valid }
 	end
 
@@ -87,15 +116,15 @@ describe User do
 	end
 
   describe "when email format is invalid" do
-    it "should be invalid" do
+     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
+        end
       end
     end
-  end
 
   describe "when email format is valid" do
     it "should be valid" do
@@ -103,19 +132,19 @@ describe User do
       addresses.each do |valid_address|
         @user.email = valid_address
         expect(@user).to be_valid
+        end
       end
     end
-  end
 
-  describe "When email address is already taken" do
-  	before do
-  		user_with_same_email = @user.dup
-  		user_with_same_email.email = @user.email.upcase
-  		user_with_same_email.save
-  	end
+    describe "When email address is already taken" do
+	  	before do
+	  		user_with_same_email = @user.dup
+	  		user_with_same_email.email = @user.email.upcase
+	  		user_with_same_email.save
+	  	end
 
-  	it { should_not be_valid }
-  end
+  		it { should_not be_valid }
+    end
 
   	describe "remember token" do
       before { @user.save }
@@ -149,11 +178,22 @@ describe User do
 	      let(:unfollowed_post) do
 	        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
 	      end
+	      let(:followed_user) { FactoryGirl.create(:user) }
+
+	      before do
+	        @user.follow!(followed_user)
+	        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+	      end
 
 	      its(:feed) { should include(newer_micropost) }
 	      its(:feed) { should include(older_micropost) }
 	      its(:feed) { should_not include(unfollowed_post) }
-	    end
-
-	end
+	      its(:feed) do
+        	followed_user.microposts.each do |micropost|
+          	should include(micropost)
+        	end
+        end
+        end
+    end
 end
+
